72609842c823d94ad4425f7fe93e4df379d4c6d3
Jurjen Vorhauer <Jurjen.Vorhauer@jdriven.com>
2014-11-11 09:16:31 +0100

    Added JVM memory metrics.

--- src/main/java/nl/ing/oia/tomcat/MetricsValve.java
+++ src/main/java/nl/ing/oia/tomcat/MetricsValve.java
@@ -3,8 +3,9 @@ package nl.ing.oia.tomcat;
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
-import com.codahale.metrics.graphite.Graphite;
-import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import metrics_influxdb.Influxdb;
+import metrics_influxdb.InfluxdbReporter;
 import org.apache.catalina.AccessLog;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleState;
@@ -28,33 +29,34 @@ public class MetricsValve extends ValveBase implements AccessLog {
 
 	private static final Log logger = LogFactory.getLog(MetricsValve.class);
 	private static final MetricRegistry registry = new MetricRegistry();
-	private static final Graphite graphite = new Graphite("lrv13715.europe.intranet", 2003);
-	private static final GraphiteReporter reporter = GraphiteReporter.forRegistry(registry).
-					                                                          prefixedWith("oia.api.tst").
-																																		convertDurationsTo(TimeUnit.MILLISECONDS).
-																																		convertRatesTo(TimeUnit.SECONDS).
-																																		filter(MetricFilter.ALL).
-									                                                  build(graphite);
-	private static final Map<String, Timer> uris = new ConcurrentHashMap<String, Timer>();
+	private static final Map<String, Timer> uris = new ConcurrentHashMap<>();
 
 	protected boolean requestAttributesEnabled = false;
+	private final InfluxdbReporter reporter;
 
-	public MetricsValve() {
+	public MetricsValve() throws Exception {
 		super(true);
+
+		registry.registerAll(new MemoryUsageGaugeSet());
+		final Influxdb influxdb = new Influxdb("127.0.0.1", 8086, "test", "root", "root");
+		reporter = InfluxdbReporter.forRegistry(registry).
+															  prefixedWith("test").
+																convertDurationsTo(TimeUnit.MILLISECONDS).
+																convertRatesTo(TimeUnit.MILLISECONDS).
+																filter(MetricFilter.ALL).
+		                            build(influxdb);
 		reporter.start(1, TimeUnit.MINUTES);
 		logger.info("graphite reporter started");
 	}
 
 	@Override
 	public void invoke(final Request request, final Response response) throws IOException, ServletException {
-		logger.info("invoke: uri: " + request.getDecodedRequestURI());
-
 		getNext().invoke(request, response);
 	}
 
 	@Override
 	public void log(final Request request, final Response response, final long l) {
-		final String uri = request.getDecodedRequestURI().replaceAll("/", "\\.");
+		final String uri = cleanupUri(request.getDecodedRequestURI());
 		logger.info("log: " + uri + " took: " + l);
 		final Timer timer = timerForUri(uri);
 		final Timer.Context ctx = timer.time();
@@ -62,11 +64,17 @@ public class MetricsValve extends ValveBase implements AccessLog {
 		ctx.stop();
 	}
 		private Timer timerForUri(final String uri) {
+			assert uri != null;
 			if (!uris.containsKey(uri)) {
 				final Timer timer = registry.timer(uri);
 				uris.put(uri, timer);
 			}
 			return uris.get(uri);
+		}
+		private String cleanupUri(final String in) {
+			assert in != null;
+			final String dotted = in.replaceAll("/", "\\.");
+			return dotted.equals(".") ? "ROOT" : dotted.substring(1);
 		}
 
 	@Override
